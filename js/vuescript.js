7ff21e9557dbcf4e8ec4661435dbda29e9edd9a0
igor <shu.i@mail.ru>
2016-12-24 16:09:42 +0200

    add tanks

--- js/vuescript.js
+++ js/vuescript.js
@@ -16,11 +16,64 @@ new Vue({
         showUsers: false,
         inputName: '',
         tankArr: [],
+        nations: [],
+
+        nationsUSSR: [],
+        nationsGermany: [],
+        nationsUSA: [],
+        nationsUK: [],
+        nationsFrance: [],
+        nationsCzech: [],
+        nationsSweden: [],
+        nationsJapan: [],
+        nationsChina: [],
+
        
         
     
     },
     methods: {
+        sortTanks: function(){
+            var a = this.tankArr
+            //germany usa france uk czech sweden japan china
+            for(var key in a){
+                switch(a[key].nation) {
+                    case 'ussr':  
+                       this.nationsUSSR.push(a[key])
+                       break;
+                    case 'germany':
+                        this.nationsGermany.push(a[key])
+                        break;
+                    case 'usa':
+                        this.nationsUSA.push(a[key])
+                        break;
+                    case 'france':
+                        this.nationsFrance.push(a[key])
+                        break;
+                    case 'uk':
+                        this.nationsUK.push(a[key])
+                        break;
+                    case 'czech':
+                        this.nationsSweden.push(a[key])
+                        break;
+                    case 'sweden':
+                        this.nationsSweden.push(a[key])
+                        break;
+                    case 'japan':
+                        this.nationsJapan.push(a[key])
+                        break;
+                    case 'china':
+                        this.nationsChina.push(a[key])
+                        break;
+                }
+                console.log(a[key]);
+            }
+            console.log(this.nationsUSSR.nation_i18n);
+            
+        
+
+            
+        },
         getDay : function(date, exactly){
             function converDate(date){
                 var w = new Date(+date * 1000)
@@ -76,7 +129,8 @@ new Vue({
             this.$http.get(this.listTanks).then(function(respons){
                 var dataObj = respons.data.data
                 this.tankArr = dataObj;
-                console.log(this.tankArr);
+                this.sortTanks();
+                // console.log(this.tankArr);
             })
         }
         
